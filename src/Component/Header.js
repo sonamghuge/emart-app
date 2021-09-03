593ef11b460220b3caf90726257af0c8450731b6
sonamghuge <55788564+sonamghuge@users.noreply.github.com>
2021-09-03 12:14:59 +0530

    sonam

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -4,8 +4,30 @@ class Body extends React.Component{
     render(){
         return(
             <div>
-                
+            <div className ="container">
+                <div className="row">
+                    <div className="col-2 bg-light">
+                        <h1>This is col 2</h1>
+                    </div>
+                    <div className="col-4 bg-dark">
+                        <h1>This is col 4</h1>
+                    </div>
+                    <div className="col-6 bg-light">
+                        <h1>This is col 6</h1>
+                    </div>
+                  </div>
+                  </div>     
+            <div class="m-auto bg-dark">
+                <h1>Margin and padding</h1>
             </div>
+              <ol className="list-unstyled list-inline">
+                  <li className="list-inline-item">home</li>
+                  <li className="list-inline-item">about</li>
+                  <li className="list-inline-item">contact</li>
+                  <li className="list-inline-item">blog</li>
+              </ol>
+              </div>  
+              
         )
     }
 }
